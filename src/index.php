abcc1eeffc768c6a5aa27b3adcb726046ef5b4ea
flinks <luiz.flinks@gmail.com>
2017-08-24 19:00:46 +0000

    v1

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>GBM sample app</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>This is the initial version</p>
+                <p>This is the version 1.0</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
