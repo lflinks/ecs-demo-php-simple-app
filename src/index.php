330d9c4ee5030a75cc6c0f12e28080db47247af0
flinks <luiz.flinks@gmail.com>
2017-08-30 16:13:13 +0000

    digital deploy demo

--- src/index.php
+++ src/index.php
@@ -15,10 +15,10 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>GBM sample app v1.1</h1>
+                <h1>GBM sample app v1.2</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>This is the version 1.1</p>
+                <p>This is the version 1.2</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
